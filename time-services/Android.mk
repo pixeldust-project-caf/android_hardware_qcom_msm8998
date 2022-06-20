dbaae0482c99a57348928a2d9455ef67f9ae3cd5
spezi77 <spezi7713@gmx.net>
2022-06-20 20:30:05 +0200

    msm8998: Guard for time-services (PD CAF).
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- time-services/Android.mk
+++ time-services/Android.mk
@@ -3,6 +3,8 @@
 # @brief Makefile for installing  time services header on Android.
 ################################################################################
 
+ifeq (,$(filter caf, $(BUILD_VERSION_CODE)))
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -12,3 +14,5 @@ LOCAL_COPY_HEADERS_TO := time-services
 LOCAL_COPY_HEADERS := ./time_genoff.h
 
 include $(BUILD_COPY_HEADERS)
+
+endif #BUILD_VERSION_CODE
